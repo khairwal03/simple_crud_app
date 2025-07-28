0b5352f143cdfb6f4e6a8a09975548a5fabbbb58
Manish Yadav <khairwal03@gmail.com>
2025-07-28 10:14:42 +0000

    Soft delete/restore flow: buttons, trashed view, hard delete fix, Bootstrap styling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,26 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: %i[show edit update destroy soft_delete restore]
 
   def index
-    @items = Item.all
+    @items = Item.where(soft_deleted: false)
   end
 
-  def show
+  def trashed
+    @items = Item.where(soft_deleted: true)
   end
+
+  def show; end
 
   def new
     @item = Item.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to @item, notice: 'Item was successfully created.'
+      redirect_to items_path, notice: "Item was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -26,15 +28,27 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to @item, notice: 'Item was successfully updated.'
+      redirect_to items_path, notice: "Item was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
+  # HARD delete
   def destroy
     @item.destroy
-    redirect_to items_url, notice: 'Item was successfully destroyed.'
+    redirect_back fallback_location: items_path, notice: "Item was permanently deleted."
+  end
+
+  # SOFT delete
+  def soft_delete
+    @item.update(soft_deleted: true)
+    redirect_to items_path, notice: "Item was moved to trash."
+  end
+
+  def restore
+    @item.update(soft_deleted: false)
+    redirect_to trashed_items_path, notice: "Item was restored."
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  scope :alive,   -> { where(soft_deleted: false) }
+  scope :trashed, -> { where(soft_deleted: true) }
 end
